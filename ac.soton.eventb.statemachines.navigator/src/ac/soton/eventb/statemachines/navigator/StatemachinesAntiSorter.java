bca5294c1f317db91b7bd9b4145974295d1a6562
Vitaly Savicks <snursmumrik@users.sourceforge.net>
2011-03-08 15:52:20 +0000

    Display statemachines as first elements in a machine.
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk/iumlb@10956 1434b563-b632-4741-aa49-43a3a8374d2e

--- ac.soton.eventb.statemachines.navigator/src/ac/soton/eventb/statemachines/navigator/StatemachinesAntiSorter.java
+++ ac.soton.eventb.statemachines.navigator/src/ac/soton/eventb/statemachines/navigator/StatemachinesAntiSorter.java
@@ -10,13 +10,13 @@ package ac.soton.eventb.statemachines.navigator;
 
 import org.eclipse.jface.viewers.Viewer;
 
-import ac.soton.eventb.statemachines.AbstractStatemachine;
+import ac.soton.eventb.statemachines.DiagramRoot;
 import ac.soton.eventb.statemachines.diagram.navigator.StatemachinesDomainNavigatorItem;
 import fr.systerel.internal.explorer.navigator.sorters.AntiSorter;
 
 /**
  * Anti-sorter for statemachines navigator plug-in.
- * Acts as anti-sorter of event-b navigator, except puts statemachine collection as the first element within a machine.
+ * Acts as anti-sorter of event-b navigator, except puts statemachine diagrams as the firs elements within a machine.
  * 
  * @author vitaly
  *
@@ -29,8 +29,9 @@ public class StatemachinesAntiSorter extends AntiSorter {
 	 */
 	@Override
 	public int compare(Viewer viewer, Object e1, Object e2) {
-		if (e2 instanceof StatemachinesDomainNavigatorItem)
-			if (((StatemachinesDomainNavigatorItem) e2).getEObject() instanceof AbstractStatemachine)
+		// just move statemachine diagrams to top of other items, don't sort them amongst themselves
+		if (e2 instanceof StatemachinesDomainNavigatorItem && e1 instanceof StatemachinesDomainNavigatorItem == false)
+			if (((StatemachinesDomainNavigatorItem) e2).getEObject() instanceof DiagramRoot)
 				return 1;
 		return super.compare(viewer, e1, e2);
 	}
