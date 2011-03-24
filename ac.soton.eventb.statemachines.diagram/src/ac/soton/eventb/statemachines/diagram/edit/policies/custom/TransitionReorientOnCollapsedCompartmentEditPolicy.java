0f9740f21aad0ace8af8fbe77c8137120bd5989d
Vitaly Savicks <snursmumrik@users.sourceforge.net>
2011-03-24 17:01:04 +0000

    Transition reorientation edit policy fixed.
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk/iumlb@11087 1434b563-b632-4741-aa49-43a3a8374d2e

--- ac.soton.eventb.statemachines.diagram/src/ac/soton/eventb/statemachines/diagram/edit/policies/custom/TransitionReorientOnCollapsedCompartmentEditPolicy.java
+++ ac.soton.eventb.statemachines.diagram/src/ac/soton/eventb/statemachines/diagram/edit/policies/custom/TransitionReorientOnCollapsedCompartmentEditPolicy.java
@@ -13,8 +13,11 @@ import java.util.Set;
 
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.gef.EditPart;
+import org.eclipse.gmf.runtime.diagram.core.commands.DeleteCommand;
 import org.eclipse.gmf.runtime.diagram.core.util.ViewUtil;
+import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.CanonicalEditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
@@ -36,34 +39,63 @@ import ac.soton.eventb.statemachines.Transition;
  */
 public class TransitionReorientOnCollapsedCompartmentEditPolicy extends CanonicalListenerEditPolicy {
 
+	/**
+	 * Edge type.
+	 * 
+	 * @author vitaly
+	 *
+	 */
+	public enum EdgeType {
+		TARGET, SOURCE;
+	}
+
 	public static final String TROCC_ROLE = "TROCC_ROLE";
-	private static final int SOURCE = 1;
-	private static final int TARGET = 2;
 
 	@Override
 	protected void handleNotificationEvent(Notification notification) {
 		Object feature = notification.getFeature();
 		if (feature.equals(NotationPackage.eINSTANCE.getDrawerStyle_Collapsed())) {
 			GraphicalEditPart host = (GraphicalEditPart) getHost();
-			View parentView = (View) host.getParent().getModel();
+			View view = (View) host.getParent().getModel();
 			EventBElement container = (EventBElement) getSemanticHost();
 			
 			Set<Edge> sourceEdges = new HashSet<Edge>();
 			Set<Edge> targetEdges = new HashSet<Edge>();
-			getAllRelatedEdgesForView2((View) parentView, sourceEdges, SOURCE);
-			getAllRelatedEdgesForView2((View) parentView, targetEdges, TARGET);
-			// set source/target of an edge either to container, or topmost containing element whose view is collapsed
-			// depending if a host part was collapsed or expanded
+			
+			findRelatedEdges((View) view, sourceEdges, EdgeType.SOURCE);
+			findRelatedEdges((View) view, targetEdges, EdgeType.TARGET);
+			
+			// set source
 			for (Edge edge : sourceEdges) {
 				Transition t = (Transition) edge.getElement();
-				if (!notification.getNewBooleanValue())
+				
+				// ignore if internal transition, i.e. target is also within container
+				if (EcoreUtil.isAncestor(view.getElement(), t.getTarget()))
+					continue;
+				
+				host.getDiagramEditDomain().getDiagramCommandStack().execute(new ICommandProxy(new DeleteCommand(edge)));
+				
+				// if expanded, find collapsed source or original
+				if (notification.getNewBooleanValue() == false)
 					container = (EventBElement) findTopCollapsedContainer(host, t.getSource());
+				
 				t.setSourceContainer(container);
 			}
+			
+			// set target
 			for (Edge edge : targetEdges) {
 				Transition t = (Transition) edge.getElement();
-				if (!notification.getNewBooleanValue())
+				
+				// ignore if internal transition, i.e. source is also within container
+				if (EcoreUtil.isAncestor(view.getElement(), t.getSource()))
+					continue;
+
+				host.getDiagramEditDomain().getDiagramCommandStack().execute(new ICommandProxy(new DeleteCommand(edge)));
+				
+				// if expanded, find collapsed target or original
+				if (notification.getNewBooleanValue() == false)
 					container = (EventBElement) findTopCollapsedContainer(host, t.getTarget());
+				
 				t.setTargetContainer(container);
 			}
 			// refresh the diagram
@@ -101,19 +133,25 @@ public class TransitionReorientOnCollapsedCompartmentEditPolicy extends Canonica
 
 	/**
 	 * Modified {@link org.eclipse.gmf.runtime.diagram.core.util.ViewUtil#getAllRelatedEdgesForView(View, Set)}
-	 * to support selective edge search (only source or only target edges).
+	 * to support selective edge search.
 	 * @param view a view to look in
 	 * @param allEdges a set of edges found
-	 * @param flag source or target edges
+	 * @param type edge type
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	static public void  getAllRelatedEdgesForView2(View view, Set<Edge> allEdges, int flag) {
-		if ((flag & SOURCE) != 0) allEdges.addAll(ViewUtil.getSourceConnections(view));
-		if ((flag & TARGET) != 0) allEdges.addAll(ViewUtil.getTargetConnections(view));
+	static public void  findRelatedEdges(View view, Set<Edge> allEdges, EdgeType type) {
+		if (type == EdgeType.SOURCE)
+			allEdges.addAll(ViewUtil.getSourceConnections(view));
+		else if (type == EdgeType.TARGET)
+			allEdges.addAll(ViewUtil.getTargetConnections(view));
+		else
+			return;
+		
+		// look deeper
 		for (Iterator itr = view.getChildren().iterator(); itr.hasNext();) {
 			Object obj = itr.next();
 			if (obj instanceof View) {
-				getAllRelatedEdgesForView2((View)obj, allEdges, flag);
+				findRelatedEdges((View)obj, allEdges, type);
 			}
 		}
 	}
