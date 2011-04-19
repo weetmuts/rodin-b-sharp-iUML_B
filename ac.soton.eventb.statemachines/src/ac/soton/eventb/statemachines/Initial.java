932ef96f196c6d79fc00b5612bf911c697c3b237
Vitaly Savicks <snursmumrik@users.sourceforge.net>
2011-04-19 11:29:32 +0000

    Model reverted to transitions without refinement.
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk/iumlb@11295 1434b563-b632-4741-aa49-43a3a8374d2e

--- ac.soton.eventb.statemachines/src/ac/soton/eventb/statemachines/Initial.java
+++ ac.soton.eventb.statemachines/src/ac/soton/eventb/statemachines/Initial.java
@@ -16,7 +16,7 @@ package ac.soton.eventb.statemachines;
  *
  *
  * @see ac.soton.eventb.statemachines.StatemachinesPackage#getInitial()
- * @model annotation="http://www.eclipse.org/emf/2002/Ecore constraints='hasOutgoingOnRoot\nhasOutgoingOnNestedIfExternalIncoming'"
+ * @model annotation="http://www.eclipse.org/emf/2002/Ecore constraints='hasAtMostOneOutgoing\nhasOutgoingOnRoot\nhasOutgoingOnNestedIfExternalIncoming'"
  * @generated
  */
 public interface Initial extends AbstractNode {
--- ac.soton.eventb.statemachines/src/ac/soton/eventb/statemachines/StatemachinesPackage.java
+++ ac.soton.eventb.statemachines/src/ac/soton/eventb/statemachines/StatemachinesPackage.java
@@ -405,22 +405,13 @@ public interface StatemachinesPackage extends EPackage {
 	int TRANSITION__OPERATIONS = CorePackage.EVENT_BCOMMENTED_ELEMENT_FEATURE_COUNT + 6;
 
 	/**
-	 * The feature id for the '<em><b>Refines</b></em>' reference.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 * @ordered
-	 */
-	int TRANSITION__REFINES = CorePackage.EVENT_BCOMMENTED_ELEMENT_FEATURE_COUNT + 7;
-
-	/**
 	 * The number of structural features of the '<em>Transition</em>' class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @generated
 	 * @ordered
 	 */
-	int TRANSITION_FEATURE_COUNT = CorePackage.EVENT_BCOMMENTED_ELEMENT_FEATURE_COUNT + 8;
+	int TRANSITION_FEATURE_COUNT = CorePackage.EVENT_BCOMMENTED_ELEMENT_FEATURE_COUNT + 7;
 
 	/**
 	 * The meta object id for the '{@link ac.soton.eventb.statemachines.impl.DiagramRootImpl <em>Diagram Root</em>}' class.
@@ -1729,17 +1720,6 @@ public interface StatemachinesPackage extends EPackage {
 	EAttribute getTransition_Operations();
 
 	/**
-	 * Returns the meta object for the reference '{@link ac.soton.eventb.statemachines.Transition#getRefines <em>Refines</em>}'.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @return the meta object for the reference '<em>Refines</em>'.
-	 * @see ac.soton.eventb.statemachines.Transition#getRefines()
-	 * @see #getTransition()
-	 * @generated
-	 */
-	EReference getTransition_Refines();
-
-	/**
 	 * Returns the meta object for class '{@link ac.soton.eventb.statemachines.RefinedStatemachine <em>Refined Statemachine</em>}'.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
@@ -2077,14 +2057,6 @@ public interface StatemachinesPackage extends EPackage {
 		 * @generated
 		 */
 		EAttribute TRANSITION__OPERATIONS = eINSTANCE.getTransition_Operations();
-
-		/**
-		 * The meta object literal for the '<em><b>Refines</b></em>' reference feature.
-		 * <!-- begin-user-doc -->
-		 * <!-- end-user-doc -->
-		 * @generated
-		 */
-		EReference TRANSITION__REFINES = eINSTANCE.getTransition_Refines();
 
 		/**
 		 * The meta object literal for the '{@link ac.soton.eventb.statemachines.impl.RefinedStatemachineImpl <em>Refined Statemachine</em>}' class.
