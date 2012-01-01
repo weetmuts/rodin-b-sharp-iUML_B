2576bb6694f30ad9d5c67ff72744e0fc26e5e526
Colin Snook <cfsnook@users.sourceforge.net>
2012-01-01 18:13:21 +0000

    add methods to make events, parameters and guards
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk/iumlb@13908 1434b563-b632-4741-aa49-43a3a8374d2e

--- ac.soton.eventb.emf.diagrams.generator/src/ac/soton/eventb/emf/diagrams/generator/Make.java
+++ ac.soton.eventb.emf.diagrams.generator/src/ac/soton/eventb/emf/diagrams/generator/Make.java
@@ -1,10 +1,17 @@
 package ac.soton.eventb.emf.diagrams.generator;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eventb.emf.core.EventBElement;
 import org.eventb.emf.core.machine.Action;
+import org.eventb.emf.core.machine.Convergence;
+import org.eventb.emf.core.machine.Event;
+import org.eventb.emf.core.machine.Guard;
 import org.eventb.emf.core.machine.Invariant;
 import org.eventb.emf.core.machine.MachineFactory;
+import org.eventb.emf.core.machine.Parameter;
 import org.eventb.emf.core.machine.Variable;
 
 public class Make {
@@ -30,6 +37,47 @@ public class Make {
 	    return i;  
 	}
 
+	public static Object event(String name) {
+		return event(name, false, Convergence.ORDINARY, Collections.<String> emptyList(), "");
+	}
+		
+	public static Object event(String name, boolean extended, Convergence convergence, List<String> refinesNames, String comment) {
+		Event e =  MachineFactory.eINSTANCE.createEvent();
+	    e.setName(name);
+	    e.setExtended(extended);
+	    e.setConvergence(convergence);
+	    e.getRefinesNames().addAll(refinesNames);
+	    e.setLocalGenerated(true);
+	    e.setComment(comment);
+	    return e;  
+	}
+
+	public static Object parameter(String name, String comment) {
+		Parameter p =  MachineFactory.eINSTANCE.createParameter();
+	    p.setName(name);
+	    p.setLocalGenerated(true);
+	    p.setComment(comment);
+	    return p;  
+	}
+	
+	public static Object guard(String name, String predicate){
+		return guard(name, false, predicate, "");
+	}
+	
+	public static Object guard(String name, boolean theorem, String invariant, String comment) {
+		Guard g =  MachineFactory.eINSTANCE.createGuard();
+	    g.setName(name);
+	    g.setTheorem(theorem);
+	    g.setPredicate(invariant);
+	    g.setLocalGenerated(true);
+	    g.setComment(comment);
+	    return g;  
+	}
+	
+	public static Object action(String name, String expression){
+		return action(name, expression, "");
+	}
+	
 	public static Object action(String name, String expression, String comment) {
 		Action a =  MachineFactory.eINSTANCE.createAction();
 	    a.setName(name);
