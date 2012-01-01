57c9a362f4a3c8620919dcd1a479ad9781d93a57
Colin Snook <cfsnook@users.sourceforge.net>
2012-01-01 18:12:07 +0000

    add method to find a generated element
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk/iumlb@13907 1434b563-b632-4741-aa49-43a3a8374d2e

--- ac.soton.eventb.emf.diagrams.generator/src/ac/soton/eventb/emf/diagrams/generator/Find.java
+++ ac.soton.eventb.emf.diagrams.generator/src/ac/soton/eventb/emf/diagrams/generator/Find.java
@@ -1,13 +1,32 @@
 package ac.soton.eventb.emf.diagrams.generator;
 
+import java.util.List;
+
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eventb.emf.core.EventBElement;
 import org.eventb.emf.core.EventBNamed;
 
+import ac.soton.eventb.emf.core.extension.coreextension.EventBLabeled;
+
 public class Find {
+	
+	
 
 	public static EventBNamed named(EList<? extends EventBNamed> collection, String name){
 		for (EventBNamed element : collection){
 			if (name.equals(element.getName())) return element;
+		}
+		return null;
+	}
+
+	public static Object generatedElement(List<GenerationDescriptor> generatedElements, EventBElement parent, EStructuralFeature feature, String identifier) {
+		for (GenerationDescriptor generatedElement : generatedElements){
+			if (generatedElement.parent == parent && generatedElement.feature== feature){
+				if ((generatedElement.value instanceof EventBNamed && ((EventBNamed)generatedElement.value).getName().equals(identifier))
+				|| (generatedElement.value instanceof EventBLabeled && ((EventBLabeled)generatedElement.value).getLabel().equals(identifier))) 
+					return generatedElement.value;
+			}
 		}
 		return null;
 	}
