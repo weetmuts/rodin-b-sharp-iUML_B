e55e90706fa981fa4f47e40a9afe445a4020a925
Colin Snook <cfsnook@users.sourceforge.net>
2012-01-07 12:35:33 +0000

    Find - add method for finding the project (currently returns null)
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk/iumlb@13951 1434b563-b632-4741-aa49-43a3a8374d2e

--- ac.soton.eventb.emf.diagrams.generator/src/ac/soton/eventb/emf/diagrams/generator/Find.java
+++ ac.soton.eventb.emf.diagrams.generator/src/ac/soton/eventb/emf/diagrams/generator/Find.java
@@ -1,11 +1,13 @@
 package ac.soton.eventb.emf.diagrams.generator;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eventb.emf.core.EventBElement;
 import org.eventb.emf.core.EventBNamed;
+import org.eventb.emf.core.Project;
 
 import ac.soton.eventb.emf.core.extension.coreextension.EventBLabeled;
 
@@ -30,4 +32,25 @@ public class Find {
 		}
 		return null;
 	}
+
+	/**
+	 * find the containing Project for this 
+	 * @param machine
+	 * @return
+	 * @throws IOException
+	 */
+	public static Project project(EventBElement eventBelement) throws IOException {
+//		URI eventBelementUri = eventBelement.getURI();
+//		URI projectUri = eventBelementUri.trimFragment().trimSegments(1);
+//		ProjectResource projectResource = new ProjectResource();
+//		projectResource.setURI(eventBelement.getURI());
+//		projectResource.load(null);
+//		for (EObject eObject : projectResource.getContents()){
+//			if (eObject instanceof Project){
+//				return (Project)eObject;
+//			}
+//		}
+		return null;
+	}
+
 }
