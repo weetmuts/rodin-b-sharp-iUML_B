04b905db759c4cd50d83cd91648b68e3e78fcea8
Vitaly Savicks <snursmumrik@users.sourceforge.net>
2012-01-13 15:42:37 +0000

    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk/iumlb@14001 1434b563-b632-4741-aa49-43a3a8374d2e

---
+++ ac.soton.eventb.statemachines.persistence/src/ac/soton/eventb/statemachines/persistence/StatemachinesPersistencePlugin.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2011 University of Southampton.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package ac.soton.eventb.statemachines.persistence;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class StatemachinesPersistencePlugin implements BundleActivator {
+
+	public static final String PLUGIN_ID = "ac.soton.eventb.statemachines.persistence";
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		StatemachinesPersistencePlugin.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		StatemachinesPersistencePlugin.context = null;
+	}
+
+}
