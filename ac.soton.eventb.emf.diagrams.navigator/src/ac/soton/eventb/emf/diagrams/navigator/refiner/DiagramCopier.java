940f46d3026b4bd7a463b5adf99f17e0e357bb85
Colin Snook <cfsnook@users.sourceforge.net>
2012-07-03 15:29:43 +0000

    Initial work on a diagram refiner - load source (abstract) machine in EMF and use diagram provider to find the filename of any contained diagram extensions.
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk/iumlb@14883 1434b563-b632-4741-aa49-43a3a8374d2e

--- ac.soton.eventb.emf.diagrams.navigator/src/ac/soton/eventb/emf/diagrams/navigator/refiner/DiagramCopier.java
+++ ac.soton.eventb.emf.diagrams.navigator/src/ac/soton/eventb/emf/diagrams/navigator/refiner/DiagramCopier.java
@@ -1,58 +1,69 @@
 package ac.soton.eventb.emf.diagrams.navigator.refiner;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.Map;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.emf.ecore.EObject;
-import org.eventb.core.basis.EventBElement;
-import org.eventb.emf.persistence.factory.RodinResource;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eventb.emf.core.AbstractExtension;
+import org.eventb.emf.core.machine.Machine;
 import org.rodinp.core.IInternalElement;
 import org.rodinp.core.IRefinementParticipant;
-import org.rodinp.core.IRodinElement;
 import org.rodinp.core.RodinDBException;
 
-import ac.soton.eventb.emf.core.extension.persistence.ISerialisedExtension;
 import ac.soton.eventb.emf.diagrams.navigator.DiagramsNavigatorExtensionPlugin;
 import ac.soton.eventb.emf.diagrams.navigator.provider.IDiagramProvider;
 
 public class DiagramCopier implements IRefinementParticipant {
 
+	ResourceSet resourceSet =  new ResourceSetImpl();
+	
 	@Override
 	public void process(IInternalElement targetRoot,
 			IInternalElement sourceRoot, IProgressMonitor monitor)
 			throws RodinDBException {
-		
-		IPath file = targetRoot.getPath();
-		//file.
 
-//		RodinResource emfResource = new RodinResource();
-//		emfResource.setURI(file.getLocationURI());
-//		IRodinElement[] children = getAllChildren(sourceRoot);
-//		for (IRodinElement child : children){
-//			if (child instanceof ISerialisedExtension){
-//				String serialised = ((ISerialisedExtension)child).getSerialised();
-//				
-//				if (element instanceof IAdaptable) {
-//					EObject eobject = (EObject) ((IAdaptable) element).getAdapter(EObject.class);
-//					
-//					// find diagram provider
-//					Map<String, IDiagramProvider> registry = DiagramsNavigatorExtensionPlugin.getDefault().getDiagramProviderRegistry();
-//					String type = eobject.eClass().getName();
-//					IDiagramProvider provider = registry.get(type);
-//					if (provider == null)
-//						return;
-//			}
-//		}
-		
-		
-	}
-	
+		URI fileURI = URI.createPlatformResourceURI(sourceRoot.getResource().getFullPath().toString(), true);
+		Resource resource = resourceSet.getResource(fileURI, false); //n.b. do not load until notifications disabled
+		if (resource == null){
+			resource = resourceSet.createResource(fileURI);
+		}
+		if (!resource.isLoaded()){
+			resource.eSetDeliver(false);	// turn off notifications to Transactional Change Recorder while loading
+			try {
+				resource.load(Collections.emptyMap());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			resource.eSetDeliver(true);
+		}
+		if (resource.isLoaded() && resource.getContents().isEmpty() == false && resource.getContents().get(0) instanceof Machine) {
+			Machine machine = (Machine) resource.getContents().get(0);
 
+			for (AbstractExtension ext : machine.getExtensions()){
+				// find diagram provider
+				Map<String, IDiagramProvider> registry = DiagramsNavigatorExtensionPlugin.getDefault().getDiagramProviderRegistry();
+				String type = ext.eClass().getName();
+				IDiagramProvider provider = registry.get(type);
+				// get diagram filename
+				String filename = provider ==null? null : provider.getDiagramFileName(ext);
 
-	IRodinElement[] getAllChildren (IInternalElement parent) throws RodinDBException{
-		return parent.getChildren();
+				if (filename!= null){
+					// we now have the filename of the diagram 
+					// NOTE: it is RELATIVE to the containing project
+					// Also note that this code is not specific to statemachines in any way,
+					// it should work for any diagrams that use our extensions framework and 
+					// contribute a Diagram provider (like the statemachines plugin does).
+					System.out.println("diagram file = "+ filename);
+					
+					//TODO: WANG TO TAKE IT FROM HERE!!!
+					
+				}
+			}	
+		}		
 	}
 }
