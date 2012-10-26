fb9c3ef4ae24e5464a848feb42febcdffbdc23a0
Colin Snook <cfsnook@users.sourceforge.net>
2012-10-26 11:59:08 +0000

    no changes - artifact of regeneration
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk/iumlb@15230 1434b563-b632-4741-aa49-43a3a8374d2e

--- ac.soton.eventb.classdiagrams.diagram/src/ac/soton/eventb/classdiagrams/diagram/part/ClassdiagramsVisualIDRegistry.java
+++ ac.soton.eventb.classdiagrams.diagram/src/ac/soton/eventb/classdiagrams/diagram/part/ClassdiagramsVisualIDRegistry.java
@@ -13,7 +13,6 @@ import ac.soton.eventb.classdiagrams.diagram.edit.parts.AssociationFunctionalTot
 import ac.soton.eventb.classdiagrams.diagram.edit.parts.AssociationNameEditPart;
 import ac.soton.eventb.classdiagrams.diagram.edit.parts.AssociationSurjectiveInjectiveEditPart;
 import ac.soton.eventb.classdiagrams.diagram.edit.parts.ClassAttributeEditPart;
-import ac.soton.eventb.classdiagrams.diagram.edit.parts.ClassAttributeNameEditPart;
 import ac.soton.eventb.classdiagrams.diagram.edit.parts.ClassAttributesEditPart;
 import ac.soton.eventb.classdiagrams.diagram.edit.parts.ClassEditPart;
 import ac.soton.eventb.classdiagrams.diagram.edit.parts.ClassNameEditPart;
