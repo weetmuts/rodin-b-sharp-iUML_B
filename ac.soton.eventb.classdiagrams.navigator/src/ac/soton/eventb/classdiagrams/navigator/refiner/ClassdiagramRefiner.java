c161cf2dbf6800e2b367f63b0abd7964af964cef
Colin Snook <cfsnook@users.sourceforge.net>
2012-11-11 22:28:54 +0000

    Change to use generic Event-B EMF refiner
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk/iumlb@15276 1434b563-b632-4741-aa49-43a3a8374d2e

---
+++ ac.soton.eventb.classdiagrams.navigator/src/ac/soton/eventb/classdiagrams/navigator/refiner/ClassdiagramRefiner.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright (c) 2012 University of Southampton.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package ac.soton.eventb.classdiagrams.navigator.refiner;
+
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EReference;
+import org.eventb.emf.core.machine.MachinePackage;
+
+import ac.soton.eventb.classdiagrams.ClassdiagramsPackage;
+import ac.soton.eventb.emf.core.extension.navigator.refiner.AbstractExtensionRefiner;
+
+/**
+ * Class diagram Refiner 
+ * 
+ * @author cfsnook
+ *
+ */
+public class ClassdiagramRefiner extends AbstractExtensionRefiner {
+
+	/**
+	 * populate the given list with the meta-classes that the refiner needs to filter out
+	 *  from the copy for statemachine refinement.
+	 * (e.g. state invariants)
+	 */
+	@Override
+	protected void populateFilterByTypeList(final List<EClass> filterList){
+		filterList.add(MachinePackage.Literals.INVARIANT);
+	}
+	
+	/**
+	 * populate the given map with the reference features that the refiner needs to copy for statemachine refinement.
+	 * This is refines (as references to their abstract counterparts) and
+	 * elaborates, incoming, outgoing, source and target (as intra-level references) 
+	 */
+	@Override
+	protected void populateReferenceMap(final Map<EReference,Boolean> referencemap){
+		referencemap.put(ClassdiagramsPackage.Literals.CLASS__REFINES, true);
+		referencemap.put(ClassdiagramsPackage.Literals.CLASSDIAGRAM__REFINES, true);	
+		referencemap.put(ClassdiagramsPackage.Literals.ELABORATIVE_ELEMENT__ELABORATES, false);
+		referencemap.put(ClassdiagramsPackage.Literals.ASSOCIATION__SOURCE, false);
+		referencemap.put(ClassdiagramsPackage.Literals.ASSOCIATION__TARGET, false);
+		referencemap.put(ClassdiagramsPackage.Literals.CLASS__INCOMING, false);
+		referencemap.put(ClassdiagramsPackage.Literals.CLASS__OUTGOING, false);
+		referencemap.put(ClassdiagramsPackage.Literals.CLASSDIAGRAM__CLASS_ASSOCIATIONS, false);
+	}
+
+/**
+ * returns the Components Extension ID
+ */
+	@Override
+	protected String getExtensionID() {
+		return ClassdiagramsPackage.CLASSDIAGRAMS_EXTENSION_ID;
+	}
+
+}
