34389be620b17c35f17e51893ca415bff20e5aee
Colin Snook <cfsnook@users.sourceforge.net>
2013-01-18 19:22:43 +0000

    move generate command type into its own class
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk/iumlb@15459 1434b563-b632-4741-aa49-43a3a8374d2e

--- ac.soton.eventb.emf.diagrams.generator/src/ac/soton/eventb/emf/diagrams/generator/actions/GenerateAction.java
+++ ac.soton.eventb.emf.diagrams.generator/src/ac/soton/eventb/emf/diagrams/generator/actions/GenerateAction.java
@@ -8,39 +8,24 @@
 
 package ac.soton.eventb.emf.diagrams.generator.actions;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Collections;
-import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.expressions.EvaluationContext;
-import org.eclipse.core.resources.IWorkspaceRunnable;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.emf.common.util.WrappedException;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.transaction.Transaction;
-import org.eclipse.emf.transaction.TransactionalEditingDomain;
-import org.eclipse.gmf.runtime.common.core.command.CommandResult;
 import org.eclipse.gmf.runtime.diagram.ui.resources.editor.parts.DiagramDocumentEditor;
-import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eventb.emf.core.EventBElement;
-import org.rodinp.core.RodinCore;
-import org.rodinp.core.RodinDBException;
 
 import ac.soton.eventb.emf.diagrams.generator.Activator;
-import ac.soton.eventb.emf.diagrams.generator.impl.Generator;
+import ac.soton.eventb.emf.diagrams.generator.command.GenerateCommand;
 import ac.soton.eventb.emf.diagrams.generator.impl.GeneratorFactory;
 import ac.soton.eventb.emf.diagrams.generator.impl.Messages;
 
@@ -135,103 +120,5 @@ public class GenerateAction extends AbstractHandler {
 			}
 		}
 		return null;
-	}
-
-	
-
-	//////////////////////GENERATE COMMAND//////////////////////////
-	private static class GenerateCommand extends AbstractTransactionalCommand {
-
-		private EventBElement element;
-
-		/**
-		 * @param domain
-		 * @param label
-		 * @param affectedFiles
-		 */
-		public GenerateCommand(TransactionalEditingDomain editingDomain, EventBElement element) {
-			super(editingDomain, Messages.GENERATOR_MSG_11, null);
-			setOptions(Collections.singletonMap(Transaction.OPTION_UNPROTECTED, Boolean.TRUE));
-			this.element = element;
-		}
-		
-		@Override
-		public boolean canRedo(){
-			return false;
-		}
-
-		@Override
-		public boolean canUndo(){
-			return false;
-		}
-
-		/* (non-Javadoc)
-		 * @see org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand#doExecuteWithResult(org.eclipse.core.runtime.IProgressMonitor, org.eclipse.core.runtime.IAdaptable)
-		 */
-		@Override
-		protected CommandResult doExecuteWithResult(IProgressMonitor monitor,
-				IAdaptable info) throws ExecutionException {
-			
-			try {
-				
-				RodinCore.run(new IWorkspaceRunnable() {
-					public void run(final IProgressMonitor monitor) throws CoreException{
-						TransactionalEditingDomain editingDomain = getEditingDomain();
-						final List<Resource> modifiedResources;
-						
-						monitor.beginTask(Messages.GENERATOR_MSG_12,10);		
-						monitor.setTaskName(Messages.GENERATOR_MSG_13(element)); 
-
-						// flush the command stack as this is unprotected and has no undo/redo
-						editingDomain.getCommandStack().flush();
-						monitor.worked(1);
-				        monitor.subTask(Messages.GENERATOR_MSG_14);
-						//try to create an appropriate generator
-						Generator generator = GeneratorFactory.getFactory().createGenerator(element.eClass());
-						monitor.worked(2);
-				        monitor.subTask(Messages.GENERATOR_MSG_15);
-				        
-				        //try to run the generation
-						modifiedResources = generator.generate(editingDomain,element);
-						monitor.worked(4);
-						if (modifiedResources == null){
-							
-							//ErrorDialog errorDialog = new ErrorDialog(diagramEditor.getSite().getShell(), label, label, null, 0); 
-							//should display a message here 
-							//"Generation Failed - see error log for details"
-							
-						}else{
-						
-							//try to save all the modified resources
-					        monitor.subTask(Messages.GENERATOR_MSG_16);
-							for (Resource resource : modifiedResources){
-								try {
-									resource.save(Collections.emptyMap());
-									monitor.worked(1);
-								} catch (IOException e) {
-									//throw this as a CoreException
-									throw new CoreException(
-											new Status(Status.ERROR, Activator.PLUGIN_ID, Messages.GENERATOR_MSG_18(resource), e));
-								}					
-							}
-							
-						}
-					monitor.done();
-					}
-				},monitor);
-				
-				return CommandResult.newOKCommandResult();
-
-			} catch (RodinDBException e) {
-				Activator.logError(Messages.GENERATOR_MSG_19, e);
-				return CommandResult.newErrorCommandResult(e);
-			} catch (WrappedException e) {
-				Activator.logError(Messages.GENERATOR_MSG_20, e);
-				return CommandResult.newErrorCommandResult(e);
-
-			} finally {
-				monitor.done();
-			}
-		}
 	}
 }
---
+++ ac.soton.eventb.emf.diagrams.generator/src/ac/soton/eventb/emf/diagrams/generator/command/GenerateCommand.java
@@ -0,0 +1,122 @@
+package ac.soton.eventb.emf.diagrams.generator.command;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.WrappedException;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.transaction.Transaction;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.gmf.runtime.common.core.command.CommandResult;
+import org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand;
+import org.eventb.emf.core.EventBElement;
+import org.rodinp.core.RodinCore;
+import org.rodinp.core.RodinDBException;
+
+import ac.soton.eventb.emf.diagrams.generator.Activator;
+import ac.soton.eventb.emf.diagrams.generator.impl.Generator;
+import ac.soton.eventb.emf.diagrams.generator.impl.GeneratorFactory;
+import ac.soton.eventb.emf.diagrams.generator.impl.Messages;
+
+//////////////////////GENERATE COMMAND//////////////////////////
+public class GenerateCommand extends AbstractTransactionalCommand {
+
+	private EventBElement element;
+
+	/**
+	 * @param domain
+	 * @param label
+	 * @param affectedFiles
+	 */
+	public GenerateCommand(TransactionalEditingDomain editingDomain, EventBElement element) {
+		super(editingDomain, Messages.GENERATOR_MSG_11, null);
+		setOptions(Collections.singletonMap(Transaction.OPTION_UNPROTECTED, Boolean.TRUE));
+		this.element = element;
+	}
+	
+	@Override
+	public boolean canRedo(){
+		return false;
+	}
+
+	@Override
+	public boolean canUndo(){
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.gmf.runtime.emf.commands.core.command.AbstractTransactionalCommand#doExecuteWithResult(org.eclipse.core.runtime.IProgressMonitor, org.eclipse.core.runtime.IAdaptable)
+	 */
+	@Override
+	protected CommandResult doExecuteWithResult(IProgressMonitor monitor,
+			IAdaptable info) throws ExecutionException {
+		
+		try {
+			
+			RodinCore.run(new IWorkspaceRunnable() {
+				public void run(final IProgressMonitor monitor) throws CoreException{
+					TransactionalEditingDomain editingDomain = getEditingDomain();
+					final List<Resource> modifiedResources;
+					
+					monitor.beginTask(Messages.GENERATOR_MSG_12,10);		
+					monitor.setTaskName(Messages.GENERATOR_MSG_13(element)); 
+
+					// flush the command stack as this is unprotected and has no undo/redo
+					editingDomain.getCommandStack().flush();
+					monitor.worked(1);
+			        monitor.subTask(Messages.GENERATOR_MSG_14);
+					//try to create an appropriate generator
+					Generator generator = GeneratorFactory.getFactory().createGenerator(element.eClass());
+					monitor.worked(2);
+			        monitor.subTask(Messages.GENERATOR_MSG_15);
+			        
+			        //try to run the generation
+					modifiedResources = generator.generate(editingDomain,element);
+					monitor.worked(4);
+					if (modifiedResources == null){
+						
+						//ErrorDialog errorDialog = new ErrorDialog(diagramEditor.getSite().getShell(), label, label, null, 0); 
+						//should display a message here 
+						//"Generation Failed - see error log for details"
+						
+					}else{
+					
+						//try to save all the modified resources
+				        monitor.subTask(Messages.GENERATOR_MSG_16);
+						for (Resource resource : modifiedResources){
+							try {
+								resource.save(Collections.emptyMap());
+								monitor.worked(1);
+							} catch (IOException e) {
+								//throw this as a CoreException
+								throw new CoreException(
+										new Status(Status.ERROR, Activator.PLUGIN_ID, Messages.GENERATOR_MSG_18(resource), e));
+							}					
+						}
+						
+					}
+				monitor.done();
+				}
+			},monitor);
+			
+			return CommandResult.newOKCommandResult();
+
+		} catch (RodinDBException e) {
+			Activator.logError(Messages.GENERATOR_MSG_19, e);
+			return CommandResult.newErrorCommandResult(e);
+		} catch (WrappedException e) {
+			Activator.logError(Messages.GENERATOR_MSG_20, e);
+			return CommandResult.newErrorCommandResult(e);
+
+		} finally {
+			monitor.done();
+		}
+	}
+}
