f416993463a147c6355112092d69d5e305d559cc
Colin Snook <cfsnook@users.sourceforge.net>
2013-04-05 19:28:59 +0000

    refactored diagram copier refinement participant to make use of new utilities
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk/iumlb@15552 1434b563-b632-4741-aa49-43a3a8374d2e

--- ac.soton.eventb.emf.diagrams.navigator/src/ac/soton/eventb/emf/diagrams/navigator/refiner/DiagramCopier.java
+++ ac.soton.eventb.emf.diagrams.navigator/src/ac/soton/eventb/emf/diagrams/navigator/refiner/DiagramCopier.java
@@ -7,33 +7,17 @@
  */
 package ac.soton.eventb.emf.diagrams.navigator.refiner;
 
-import java.io.BufferedReader;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.Map;
 
-import org.eclipse.core.resources.IContainer;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eventb.emf.core.AbstractExtension;
-import org.eventb.emf.core.machine.Machine;
+import org.eventb.emf.core.EventBElement;
 import org.rodinp.core.IInternalElement;
 import org.rodinp.core.IRefinementParticipant;
 import org.rodinp.core.RodinDBException;
 
+import ac.soton.eventb.emf.diagrams.navigator.DiagramUtil;
 import ac.soton.eventb.emf.diagrams.navigator.DiagramsNavigatorExtensionPlugin;
 import ac.soton.eventb.emf.diagrams.navigator.provider.IDiagramProvider;
 
@@ -46,108 +30,33 @@ import ac.soton.eventb.emf.diagrams.navigator.provider.IDiagramProvider;
  *
  */
 public class DiagramCopier implements IRefinementParticipant {
-
-	ResourceSet resourceSet =  new ResourceSetImpl();
 	
 	@Override
-	public void process(IInternalElement targetRoot,
-			IInternalElement sourceRoot, IProgressMonitor monitor)
-			throws RodinDBException {
+	public void process(IInternalElement targetRoot, IInternalElement sourceRoot, IProgressMonitor monitor) throws RodinDBException {
 
-		URI fileURI = URI.createPlatformResourceURI(sourceRoot.getResource().getFullPath().toString(), true);
-		Resource resource = resourceSet.getResource(fileURI, false); //n.b. do not load until notifications disabled
-		if (resource == null){
-			resource = resourceSet.createResource(fileURI);
-		}
-		if (!resource.isLoaded()){
-			resource.eSetDeliver(false);	// turn off notifications to Transactional Change Recorder while loading
-			try {
-				resource.load(Collections.emptyMap());
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			resource.eSetDeliver(true);
-		}
-		if (resource.isLoaded() && resource.getContents().isEmpty() == false && resource.getContents().get(0) instanceof Machine) {
-			Machine machine = (Machine) resource.getContents().get(0);
+		String oldRootName = sourceRoot.getElementName(); 
+		String newRootName = targetRoot.getElementName();
+		String fileExtension = sourceRoot.getResource().getFileExtension();		
 
-			for (AbstractExtension ext : machine.getExtensions()){
-				// find diagram provider
-				Map<String, IDiagramProvider> registry = DiagramsNavigatorExtensionPlugin.getDefault().getDiagramProviderRegistry();
-				String type = ext.eClass().getName();
-				IDiagramProvider provider = registry.get(type);
+		EventBElement eventBElement = DiagramUtil.loadIntoEMF(sourceRoot);
+		if (eventBElement != null){
+			//get the diagram provider registry
+			Map<String, IDiagramProvider> registry = DiagramsNavigatorExtensionPlugin.getDefault().getDiagramProviderRegistry();
+			//find any extension elements likely to have diagrams
+			for (AbstractExtension extension : eventBElement.getExtensions()){
+				// try to find a diagram provider
+				IDiagramProvider provider = registry.get(extension.eClass().getName());
 				if (provider!= null){
-					// get diagram filename
-					String sourceDiagramFilename = provider.getDiagramFileName(ext);
-					String projectFullPath = sourceRoot.getResource().getProject().getFullPath().toString();
-					String sourceMachineName = fileNameProcess(sourceRoot.getResource().getName());//It is the machine name, not the machine file name.
-					String targetMachineName = fileNameProcess(targetRoot.getResource().getName());
-					String targetDiagramFilename = sourceDiagramFilename.replaceFirst( sourceMachineName, targetMachineName);
+					//try to make a copy of the diagram
 					try {
-						copyDiagram(projectFullPath, sourceDiagramFilename, targetDiagramFilename, monitor);
+						DiagramUtil.copyDiagramForNewRoot(sourceRoot.getResource().getProject(), provider.getDiagramFileName(extension), oldRootName, newRootName, fileExtension, monitor);
 					} catch (CoreException e) {
-						// TODO Auto-generated catch block
+
 						e.printStackTrace();
 					}
 				}
-			}	
+			}
 		}		
 	}
-	
-	private void copyDiagram(
-			String projectFullPath,
-			String sourceFileName,
-			String targetFileName,
-			IProgressMonitor monitor)
-			throws CoreException {
-			// copy the file
-			monitor.beginTask("Copying " + sourceFileName, 1);
-			IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-			IResource resource = root.findMember(new Path(projectFullPath));
-			IContainer container = (IContainer) resource;
-			final IFile sourceFile = container.getFile(new Path(sourceFileName));
-			final IFile targetFile = container.getFile(new Path(targetFileName));
-			try {
-				InputStream stream = fileContentStream(sourceFile, targetFileName);
-				if (targetFile.exists()) {
-					targetFile.setContents(stream, true, true, monitor);
-				} else {
-					targetFile.create(stream, true, monitor);
-				}
-				stream.close();
-			} catch (IOException e) {
-			}
-			monitor.worked(1);
-		}
 
-		private InputStream fileContentStream(IFile sourceFile, String targetFile) throws IOException, CoreException {
-			BufferedReader sourceBuf = new BufferedReader(new InputStreamReader(sourceFile.getContents()));
-			String line;
-			String contents = "";
-			String source = sourceFile.getName();
-			while((line = sourceBuf.readLine()) != null){
-				contents = contents + contentProcess(line, fileNameProcess(source), fileNameProcess(targetFile)) + "\n";
-			}
-			sourceBuf.close();
-			
-			return new ByteArrayInputStream(contents.toString().getBytes());
-		}
-		
-		private String fileNameProcess(String fileName){
-			int i = 0;
-			while(!fileName.substring(i,i+1).equals(".")){
-				i++;
-			}
-			return fileName.substring(0, i).trim();
-
-		}
-		
-		private String contentProcess(String content, String source, String target){
-			String regex;
-			String replacement;
-			regex = source + ".bum";
-			replacement = target + ".bum";
-			return content.replaceAll(regex, replacement);
-
-		}
 }
