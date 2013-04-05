e4c471afd8902a0b35d88fb67b1ad233d2606786
Colin Snook <cfsnook@users.sourceforge.net>
2013-04-05 19:43:42 +0000

    Refactored DeleteDiagramCommand Handler to use new utils and other changes
    git-svn-id: svn://svn.code.sf.net/p/rodin-b-sharp/svn/trunk/iumlb@15554 1434b563-b632-4741-aa49-43a3a8374d2e

--- ac.soton.eventb.emf.diagrams.navigator/src/ac/soton/eventb/emf/diagrams/navigator/handler/DeleteDiagramElementHandler.java
+++ ac.soton.eventb.emf.diagrams.navigator/src/ac/soton/eventb/emf/diagrams/navigator/handler/DeleteDiagramElementHandler.java
@@ -9,16 +9,11 @@ package ac.soton.eventb.emf.diagrams.navigator.handler;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.Map;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.ecore.EObject;
@@ -27,15 +22,14 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.emf.transaction.util.TransactionUtil;
-import org.eclipse.emf.workspace.util.WorkspaceSynchronizer;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import ac.soton.eventb.emf.diagrams.generator.Activator;
 import ac.soton.eventb.emf.diagrams.generator.command.DeleteGeneratedCommand;
+import ac.soton.eventb.emf.diagrams.navigator.DiagramUtil;
 import ac.soton.eventb.emf.diagrams.navigator.DiagramsNavigatorExtensionPlugin;
-import ac.soton.eventb.emf.diagrams.navigator.provider.IDiagramProvider;
 
 
 /**
@@ -54,33 +48,34 @@ public class DeleteDiagramElementHandler extends AbstractHandler {
 			if (element instanceof IAdaptable) {
 				final EObject eobject = (EObject) ((IAdaptable) element).getAdapter(EObject.class);
 				final Resource resource = eobject.eResource();
-				// find diagram provider
-				Map<String, IDiagramProvider> registry = DiagramsNavigatorExtensionPlugin.getDefault().getDiagramProviderRegistry();
-				String type = eobject.eClass().getName();
-				IDiagramProvider provider = registry.get(type);
+
 				
 				if (resource != null && resource.isLoaded()) {
 					TransactionalEditingDomain editingDomain = TransactionUtil.getEditingDomain(resource);
 					if (editingDomain != null) {
-						// execute as command
-						Command cmd = new RecordingCommand(editingDomain, "Delete Diagram Command") {
-							protected void doExecute() {
-								deleteGeneratedElements(eobject);
-								EcoreUtil.delete(eobject, true);
-							}
-						};
-		
+						// command to delete the diagram element from the model
+						// TODO: could use a DeleteCommand here
+						Command cmd = 
+								new RecordingCommand(editingDomain, "Delete Diagram Command") {
+									protected void doExecute() {EcoreUtil.delete(eobject, true);}
+								};
+						//command to delete any model elements that have been generated from the diagram element
+						// (this command is provided by the generator plug-in)
+						DeleteGeneratedCommand deleteGeneratedCommand = new DeleteGeneratedCommand(editingDomain, eobject);
 						try {
 							if (cmd.canExecute()){
-								DeleteGeneratedCommand deleteGeneratedCommand = new DeleteGeneratedCommand(editingDomain, eobject);
+								//try to execute the command to delete generated elements
 								if (deleteGeneratedCommand.canExecute()){
 									deleteGeneratedCommand.execute(null, null);
 								}else{
 									Activator.logError("Failed to delete generated elements - aborted delete of : "+eobject);
 									return null;
 								}
-								deleteDiagramFile(resource, provider, eobject);
+								//delete the diagram layout file
+								DiagramUtil.deleteDiagramFile(eobject);
+								//delete the diagram model element
 								editingDomain.getCommandStack().execute(cmd);
+								//save the model
 								resource.save(Collections.emptyMap());
 							}
 						} catch (IOException e) {
@@ -91,27 +86,6 @@ public class DeleteDiagramElementHandler extends AbstractHandler {
 			}
 		}
 		return null;
-	}
-
-	protected void deleteGeneratedElements(EObject eobject) {
-		
-		
-	}
-
-	private void deleteDiagramFile(Resource resource, IDiagramProvider provider, EObject element) {
-		if (provider == null) return;
-		String filename = provider.getDiagramFileName(element);
-		IFile domainFile = WorkspaceSynchronizer.getFile(resource);
-		IProject project = domainFile.getProject();
-		IFile diagramFile = project.getFile(filename);
-		if (diagramFile.exists()) {
-			try {
-				diagramFile.delete(false,false,new NullProgressMonitor());
-			} catch (CoreException e) {
-				DiagramsNavigatorExtensionPlugin.getDefault().getLog().log(new Status(Status.ERROR, DiagramsNavigatorExtensionPlugin.PLUGIN_ID, "Failed deleting diagram File", e));
-			}
-		}
-
 	}
 
 }
