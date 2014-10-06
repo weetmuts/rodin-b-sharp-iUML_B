0a1fc9877a706991caafcffbcb50c60116cb32b1
Colin Snook <cfs@ecs.soton.ac.uk>
2014-10-06 22:42:29 +0100

    Make associations and attributes generation more consistent with class

--- ac.soton.eventb.classdiagrams.generator/src/ac/soton/eventb/classdiagrams/generator/rules/AssociationRule.java
+++ ac.soton.eventb.classdiagrams.generator/src/ac/soton/eventb/classdiagrams/generator/rules/AssociationRule.java
@@ -3,6 +3,7 @@ package ac.soton.eventb.classdiagrams.generator.rules;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eventb.emf.core.EventBElement;
@@ -47,41 +48,57 @@ protected static final EReference elaborates = CoreextensionPackage.Literals.EVE
 		Association element = (Association)sourceElement;
 		int dataKind = element.getDataKind().getValue();
 		EventBElement elaborated = (EventBElement) element.getElaborates();
-		//create element if it's a new one
-		if (elaborated==null || Is.generatedBy(elaborated, sourceElement)){
-			EventBElement newGeneratedElement = null;
-			EReference newGeneratedElementContainer = null;
-			EventBElement newGeneratedTypePredicate = null;
-			EReference newGeneratedTypePredicateContainer = null;
-			EventBElement newGeneratedInjectionPredicate = null;
-			switch (dataKind) {
-			case DataKind.SET_VALUE :
-				// should not get here - associations cannot be sets. make a constant instead.
-			case DataKind.CONSTANT_VALUE :
+		if (elaborated!=null){
+			EObject elaboratedRoot = EcoreUtil.getRootContainer(elaborated);
+			if (elaboratedRoot instanceof EventBNamedCommentedComponentElement){
+				container = (EventBNamedCommentedComponentElement)elaboratedRoot;
+			}else{
+				return ret; //can't find root
+			}
+		}else{
+			container = (EventBNamedCommentedComponentElement)EcoreUtil.getRootContainer(sourceElement);
+			if (dataKind != DataKind.VARIABLE_VALUE){
+				return ret; //NOT YET SUPPORTED
+			}
+		}
+		EventBElement newGeneratedElement = null;
+		EReference newGeneratedElementContainer = null;
+		EventBElement newGeneratedTypePredicate = null;
+		EReference newGeneratedTypePredicateContainer = null;
+		EventBElement newGeneratedInjectionPredicate = null;
+		switch (dataKind) {
+		case DataKind.SET_VALUE :
+			// should not get here - associations cannot be sets. make a constant instead.
+		case DataKind.CONSTANT_VALUE :
+			if (elaborated==null || Is.generatedBy(elaborated, sourceElement)){
 				newGeneratedElement = (EventBElement) Make.constant(element.getName(), element.getComment());
 				newGeneratedElementContainer = constants;
-				newGeneratedTypePredicate = (EventBElement) Make.axiom(Strings.ASSOCIATION_PRED_NAME(element.getName()), Strings.ASSOCIATION_PRED(element), element.getComment());
-				newGeneratedTypePredicateContainer = axioms;
-				if (!element.isFunctional() && element.isInjective()){
-					newGeneratedInjectionPredicate = (EventBElement) Make.axiom(Strings.ASSOCIATION_PRED_INJECTIVE_NAME(element.getName()), Strings.ASSOCIATION_PRED_INJECTIVE(element), element.getComment());
-				}
-				break;
-			case DataKind.VARIABLE_VALUE :
+			}
+			newGeneratedTypePredicate = (EventBElement) Make.axiom(Strings.ASSOCIATION_PRED_NAME(element.getName()), Strings.ASSOCIATION_PRED(element), element.getComment());
+			newGeneratedTypePredicateContainer = axioms;
+			if (!element.isFunctional() && element.isInjective()){
+				newGeneratedInjectionPredicate = (EventBElement) Make.axiom(Strings.ASSOCIATION_PRED_INJECTIVE_NAME(element.getName()), Strings.ASSOCIATION_PRED_INJECTIVE(element), element.getComment());
+			}
+			break;
+		case DataKind.VARIABLE_VALUE :
+			if (elaborated==null || Is.generatedBy(elaborated, sourceElement)){
 				newGeneratedElement = Make.variable(element.getName(), element.getComment());
 				newGeneratedElementContainer = variables;
-				newGeneratedTypePredicate = (EventBElement) Make.invariant(Strings.ASSOCIATION_PRED_NAME(element.getName()), Strings.ASSOCIATION_PRED(element), element.getComment());
-				newGeneratedTypePredicateContainer = invariants;
-				if (!element.isFunctional() && element.isInjective()){
-					newGeneratedInjectionPredicate = (EventBElement) Make.axiom(Strings.ASSOCIATION_PRED_INJECTIVE_NAME(element.getName()), Strings.ASSOCIATION_PRED_INJECTIVE(element), element.getComment());
-				}
-				break;
 			}
+			newGeneratedTypePredicate = (EventBElement) Make.invariant(Strings.ASSOCIATION_PRED_NAME(element.getName()), Strings.ASSOCIATION_PRED(element), element.getComment());
+			newGeneratedTypePredicateContainer = invariants;
+			if (!element.isFunctional() && element.isInjective()){
+				newGeneratedInjectionPredicate = (EventBElement) Make.axiom(Strings.ASSOCIATION_PRED_INJECTIVE_NAME(element.getName()), Strings.ASSOCIATION_PRED_INJECTIVE(element), element.getComment());
+			}
+			break;
+		}
+		if (newGeneratedElement!=null){
 			ret.add(Make.descriptor(container,newGeneratedElementContainer,newGeneratedElement,10));
 			ret.add(Make.descriptor(sourceElement, elaborates, newGeneratedElement, 10));
-			ret.add(Make.descriptor(container, newGeneratedTypePredicateContainer,newGeneratedTypePredicate, 10));
-			if (newGeneratedInjectionPredicate != null){
-				ret.add(Make.descriptor(container, newGeneratedTypePredicateContainer, newGeneratedInjectionPredicate, -1));			
-			}
+		}
+		ret.add(Make.descriptor(container, newGeneratedTypePredicateContainer,newGeneratedTypePredicate, 10));
+		if (newGeneratedInjectionPredicate != null){
+			ret.add(Make.descriptor(container, newGeneratedTypePredicateContainer, newGeneratedInjectionPredicate, -1));			
 		}
 		return ret;
 	}
