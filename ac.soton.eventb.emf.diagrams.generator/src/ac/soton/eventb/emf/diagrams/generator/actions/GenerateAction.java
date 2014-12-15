bf4ef74113274c483f98c98a37359be9b8ead5bf
Colin Snook <cfs@ecs.soton.ac.uk>
2014-12-15 14:52:55 +0000

    Corrections to enabledness checking of generate and validate actions needed in order to work with Eclipse 4.4 (Rodin 3.1.0)

--- ac.soton.eventb.emf.diagrams.generator/src/ac/soton/eventb/emf/diagrams/generator/actions/GenerateAction.java
+++ ac.soton.eventb.emf.diagrams.generator/src/ac/soton/eventb/emf/diagrams/generator/actions/GenerateAction.java
@@ -13,7 +13,6 @@ import java.lang.reflect.InvocationTargetException;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.expressions.EvaluationContext;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.gmf.runtime.diagram.ui.resources.editor.parts.DiagramDocumentEditor;
@@ -37,32 +36,6 @@ import ac.soton.eventb.emf.diagrams.generator.impl.Messages;
  */
 public class GenerateAction extends AbstractHandler {
 	
-	private boolean enabled = false;
-	@Override
-	public boolean isEnabled(){
-		return enabled && super.isEnabled();
-	}
-
-	@Override
-	public void setEnabled(Object evaluationContext) {		
-		super.setEnabled(evaluationContext);
-		enabled = false;
-		if (evaluationContext instanceof EvaluationContext){
-			EvaluationContext ec = (EvaluationContext)evaluationContext;
-			
-			Object editor = ec.getVariable("activeEditor");
-			//Object editorID = ec.getVariable("activeEditorId");
-			if (editor instanceof DiagramDocumentEditor) {
-				final DiagramDocumentEditor diagramDocumentEditor = (DiagramDocumentEditor)editor;
-				if (diagramDocumentEditor.getDiagram().getElement() instanceof EventBElement){
-					enabled =  GeneratorFactory.getFactory().canGenerate(diagramDocumentEditor.getDiagram().getElement().eClass());
-				}
-			}
-		}	
-	}
-	
-	
-
 	/* (non-Javadoc)
 	 * @see org.eclipse.core.commands.IHandler#execute(org.eclipse.core.commands.ExecutionEvent)
 	 */
@@ -72,49 +45,50 @@ public class GenerateAction extends AbstractHandler {
 		IEditorPart editor = HandlerUtil.getActiveEditorChecked(event);
 		if (editor instanceof DiagramDocumentEditor) {
 			final DiagramDocumentEditor diagramDocumentEditor = (DiagramDocumentEditor)editor;
-						
-			if (diagramDocumentEditor.getDiagram().getElement() instanceof EventBElement){
-				final EventBElement eventBElement = (EventBElement) diagramDocumentEditor.getDiagram().getElement();
-
-				// save before transformation
-				if (editor.isDirty())
-					editor.doSave(new NullProgressMonitor());
-
-				// first validate, then transform
-				if (ValidateAction.validate(diagramDocumentEditor)){
-
-					final GenerateCommand generateCommand = new GenerateCommand(
-							diagramDocumentEditor.getDiagramEditPart().getEditingDomain(), 
-							eventBElement);
-					if (generateCommand.canExecute()) {	
-						// run with progress
-						ProgressMonitorDialog dialog = new ProgressMonitorDialog(diagramDocumentEditor.getSite().getShell());
-						try {
-							dialog.run(true, true, new IRunnableWithProgress(){
-							     public void run(IProgressMonitor monitor) { 
-							    	 monitor.beginTask(Messages.GENERATOR_MSG_05, IProgressMonitor.UNKNOWN);
-							         try {
-							        	 generateCommand.execute(monitor, diagramDocumentEditor);
-							         } catch (ExecutionException e) {
-										Activator.logError(Messages.GENERATOR_MSG_06, e);
-							         }
-							         monitor.done();
-							     }
-							 });
-						} catch (InvocationTargetException e) {
-							Activator.logError(Messages.GENERATOR_MSG_07, e);
-							return null;
-						} catch (InterruptedException e) {
-							Activator.logError(Messages.GENERATOR_MSG_08, e);
-							return null;
-						} 
+			if (GeneratorFactory.getFactory().canGenerate(diagramDocumentEditor.getDiagram().getElement().eClass())){
+				if (diagramDocumentEditor.getDiagram().getElement() instanceof EventBElement){
+					final EventBElement eventBElement = (EventBElement) diagramDocumentEditor.getDiagram().getElement();
 	
-						// error feedback
-						if (false == generateCommand.getCommandResult().getStatus().isOK())
-							MessageDialog
-									.openError(editor.getSite().getShell(),
-											Messages.GENERATOR_MSG_09,
-											Messages.GENERATOR_MSG_10);
+					// save before transformation
+					if (editor.isDirty())
+						editor.doSave(new NullProgressMonitor());
+	
+					// first validate, then transform
+					if (ValidateAction.validate(diagramDocumentEditor)){
+	
+						final GenerateCommand generateCommand = new GenerateCommand(
+								diagramDocumentEditor.getDiagramEditPart().getEditingDomain(), 
+								eventBElement);
+						if (generateCommand.canExecute()) {	
+							// run with progress
+							ProgressMonitorDialog dialog = new ProgressMonitorDialog(diagramDocumentEditor.getSite().getShell());
+							try {
+								dialog.run(false, true, new IRunnableWithProgress(){
+								     public void run(IProgressMonitor monitor) { 
+								    	 monitor.beginTask(Messages.GENERATOR_MSG_05, IProgressMonitor.UNKNOWN);
+								         try {
+								        	 generateCommand.execute(monitor, diagramDocumentEditor);
+								         } catch (ExecutionException e) {
+											Activator.logError(Messages.GENERATOR_MSG_06, e);
+								         }
+								         monitor.done();
+								     }
+								 });
+							} catch (InvocationTargetException e) {
+								Activator.logError(Messages.GENERATOR_MSG_07, e);
+								return null;
+							} catch (InterruptedException e) {
+								Activator.logError(Messages.GENERATOR_MSG_08, e);
+								return null;
+							} 
+		
+							// error feedback
+							if (false == generateCommand.getCommandResult().getStatus().isOK())
+								MessageDialog
+										.openError(editor.getSite().getShell(),
+												Messages.GENERATOR_MSG_09,
+												Messages.GENERATOR_MSG_10);
+						}
 					}
 				}
 			}
