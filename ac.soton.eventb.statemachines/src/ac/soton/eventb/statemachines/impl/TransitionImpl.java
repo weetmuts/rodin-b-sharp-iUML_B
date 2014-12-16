d834e6894cafef3e29d1cdb9750b294f32d84904
Colin Snook <cfs@ecs.soton.ac.uk>
2014-12-16 18:07:11 +0000

    icons in edit plugin

--- ac.soton.eventb.statemachines/src/ac/soton/eventb/statemachines/impl/TransitionImpl.java
+++ ac.soton.eventb.statemachines/src/ac/soton/eventb/statemachines/impl/TransitionImpl.java
@@ -8,8 +8,6 @@
  */
 package ac.soton.eventb.statemachines.impl;
 
-import java.util.ArrayList;
-
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
 import org.eclipse.emf.common.util.EList;
@@ -17,7 +15,6 @@ import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eventb.emf.core.EventBElement;
-import org.eventb.emf.core.machine.Event;
 
 import ac.soton.eventb.emf.core.extension.coreextension.impl.EventBCommentedLabeledEventGroupElementImpl;
 import ac.soton.eventb.statemachines.AbstractNode;
