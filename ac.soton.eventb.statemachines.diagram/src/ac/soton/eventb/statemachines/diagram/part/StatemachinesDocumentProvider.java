a9272f94682c7b53f867286d937dab2acf7da884
Colin Snook <cfs@ecs.soton.ac.uk>
2015-06-19 14:00:41 +0100

    added some temporary diagnostic code to catch exceptions while reloading in handle element changed

--- ac.soton.eventb.statemachines.diagram/src/ac/soton/eventb/statemachines/diagram/part/StatemachinesDocumentProvider.java
+++ ac.soton.eventb.statemachines.diagram/src/ac/soton/eventb/statemachines/diagram/part/StatemachinesDocumentProvider.java
@@ -724,7 +724,13 @@ public class StatemachinesDocumentProvider extends AbstractDocumentProvider
 				// Error message to log was initially taken from org.eclipse.gmf.runtime.diagram.ui.resources.editor.ide.internal.l10n.EditorMessages.FileDocumentProvider_handleElementContentChanged
 			}
 		}
-		changedResource.unload();
+		try{
+			//FIXME: sometimes reloading changes and proxymap fails on a "Cannot modify resource set without a write transaction" exception 
+			System.out.println("***handleElementChanged - "+info.myEditorInput.getName()+" - reloading: "+changedResource.getURI().toPlatformString(true));
+			changedResource.unload();
+		}catch(Exception e){
+			System.out.println("***handleElementChanged - "+info.myEditorInput.getName()+" - FAILED RELOADING: "+changedResource.getURI().toPlatformString(true)+" Exception: "+e.getMessage());
+		}
 
 		fireElementContentAboutToBeReplaced(info.getEditorInput());
 		removeUnchangedElementListeners(info.getEditorInput(), info);
