173f31f46367d565b02b6fd09ce7f77b8102375e
Colin Snook <cfs@ecs.soton.ac.uk>
2015-09-17 14:36:35 +0100

    removed check for null editing domain - this should not be necessary

--- ac.soton.eventb.statemachines.diagram/src/ac/soton/eventb/statemachines/diagram/providers/StatemachinesValidationDecoratorProvider.java
+++ ac.soton.eventb.statemachines.diagram/src/ac/soton/eventb/statemachines/diagram/providers/StatemachinesValidationDecoratorProvider.java
@@ -126,8 +126,7 @@ public class StatemachinesValidationDecoratorProvider extends AbstractProvider
 	}
 
 	/**
-	 * check for null editing domain
-	 * @generated NOT
+	 * @generated
 	 */
 	private static void refreshDecorators(String viewId, Diagram diagram) {
 		final List decorators = viewId != null ? (List) allDecorators
@@ -136,22 +135,22 @@ public class StatemachinesValidationDecoratorProvider extends AbstractProvider
 			return;
 		}
 		final Diagram fdiagram = diagram;
-		final TransactionalEditingDomain ed = TransactionUtil
-				.getEditingDomain(fdiagram);
-		if (ed == null)
-			return;
 		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+
 			public void run() {
 				try {
-					ed.runExclusive(new Runnable() {
-						public void run() {
-							for (Iterator it = decorators.iterator(); it
-									.hasNext();) {
-								IDecorator decorator = (IDecorator) it.next();
-								decorator.refresh();
-							}
-						}
-					});
+					TransactionUtil.getEditingDomain(fdiagram).runExclusive(
+							new Runnable() {
+
+								public void run() {
+									for (Iterator it = decorators.iterator(); it
+											.hasNext();) {
+										IDecorator decorator = (IDecorator) it
+												.next();
+										decorator.refresh();
+									}
+								}
+							});
 				} catch (Exception e) {
 					StatemachinesDiagramEditorPlugin.getInstance().logError(
 							"Decorator refresh failure", e); //$NON-NLS-1$
